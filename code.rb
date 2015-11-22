771e6cab42eed7c7a2d860bcb1ea25bcd07b13a1
philiptang42 <philiptang42@gmail.com>
2015-11-21 23:58:30 -0500

    part 1 continued

--- code.rb
+++ code.rb
@@ -2,13 +2,13 @@ time_capsule = Array.new
 
 puts "Hi, this is a Time Capsule. Enter your name."
 name = gets.chomp
-puts "Hi #{name}. What do you want to add to the Time Capsule?"
-addition = gets.chomp
 
 while true
+  puts "#{name}, what do you want to add to the Time Capsule?"
+  addition = gets.chomp
+  time_capsule << addition
   puts "Here are the contents of the Time Capsule:"
   time_capsule.each do |item|
     puts "* #{item}"
   end
-  puts "What will you add to the Time Capsule?"
-  
+end
