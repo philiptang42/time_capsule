f328048d885595fa77b7ce388aa24d349953a56e
philiptang42 <philiptang42@gmail.com>
2015-11-21 23:53:19 -0500

    part 1 continued

--- code.rb
+++ code.rb
@@ -1,1 +1,6 @@
-time capsule = []
+time capsule = Array.new
+
+puts "Hi, this is a Time Capsule. Enter your name."
+name = gets.chomp
+puts "Hi #{name}. What do you want to add to the Time Capsule?"
+addition = gets.chomp
