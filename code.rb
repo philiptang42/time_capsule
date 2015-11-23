431226e92edf9e77e9bc8435d3605bb1d21d0b5e
philiptang42 <philiptang42@gmail.com>
2015-11-22 23:10:20 -0500

    part 2 begin

--- code.rb
+++ code.rb
@@ -3,6 +3,8 @@ time_capsule = Array.new
 puts "Hi, this is a Time Capsule. Enter your name."
 name = gets.chomp
 
+count = 0
+
 while true
   puts "#{name}, what do you want to add to the Time Capsule?"
   addition = gets.chomp
