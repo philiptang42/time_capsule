fcb73c00374f17630925eb678c180f3977c65dbf
philiptang42 <philiptang42@gmail.com>
2015-11-23 21:47:49 -0500

    working on part 2 from scratch

--- code.rb
+++ code.rb
@@ -1,26 +1,23 @@
 time_capsule = Array.new
 
 puts "Hi, this is a Time Capsule. Enter your name."
+puts "--------"
 name = gets.chomp
-
-item_amount = []
 
 while true
   puts "#{name}, what do you want to add to the Time Capsule?"
+  puts "Type FINISHED to stop adding things."
   addition = gets.chomp
-  if addition == "finished"
+  if addition == "FINISHED"
     puts "Here are the contents of the Time Capsule:"
     time_capsule.each do |item|
-      item_amount.each do |amount|
-        puts "* #{item} (#{amount})"
-      end
+      puts "* #{item}"
     end
     break
   else
     puts "Enter the number of #{addition} you would like to include:"
     amount = gets.chomp
     time_capsule << addition
-    item_amount << amount
     puts "#{addition} has been added!"
   end
 end
